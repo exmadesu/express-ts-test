e1b43b67ae0179c3d8c990639a1f27fac2dacfa9
exmadesu <exmadesu@gmail.com>
2025-05-08 02:05:16 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { middleware } from "./middlewares/middleware.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", middleware);
 
 app.listen(port, () => {
